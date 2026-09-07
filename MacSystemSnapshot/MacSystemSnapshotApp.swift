12905ce4545462292e2e959a84921c74768f3baa
Anna Granos <granos.anna@gmail.com>
2026-09-07 12:17:45 +0200

    Initial Commit

---
+++ MacSystemSnapshot/MacSystemSnapshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacSystemSnapshotApp.swift
+//  MacSystemSnapshot
+//
+//  Created by Anna Granos on 07/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct MacSystemSnapshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
